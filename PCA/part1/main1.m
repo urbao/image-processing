d36e4baaf11d42cb10641d12b415c019b4accd38
urbao <E24094198@gs.ncku.edu.tw>
2023-03-12 09:11:05 +0800

    Change `k` to `rk` for intuition - need to confgure how to achieve PCA ops

--- PCA/part1/main1.m
+++ PCA/part1/main1.m
@@ -1,18 +1,18 @@
 clc % clear command window
 
-k=4; % k: rank of matrix
+rk=4; % rk: rank of matrix
 % use while loop to make sure rank of matrix M is 4
 % since, most case A,B should be linearly independent, and their rank is 4
 % however, it might have some chance to have linearly dependent cases
 % and, that's why the while loop been used
 rank_X=0;
-while rank_X~=k
-        % A, B: rank must smaller or equal to 4
-        % X: rank is at most min{rank(A), rank(B)}
-        A=randi([0,1], 10, k);
-        B=randi([0,1], k, 100); 
-        X=A*B;
-        rank_X=rank(X);
+while rank_X~=rk
+    % A, B: rank must smaller or equal to 4
+    % X: rank is at most min{rank(A), rank(B)}
+    A=randi([0,1], 10, rk);
+    B=randi([0,1], rk, 100); 
+    X=A*B;
+    rank_X=rank(X);
 end
 % print out rank of X
 fprintf("Rank of X: "+rank(X)+"\n");
