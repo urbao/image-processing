d795608aa8ff879fb177a9152751bcb17e2e9837
urbao <E24094198@gs.ncku.edu.tw>
2023-03-15 00:16:03 +0800

    Finish basic function of PCA - still need to adjust sth - ex. curves, colorbar...

--- PCA/part1/main1.m
+++ PCA/part1/main1.m
@@ -13,7 +13,7 @@ while rank_X~=rk
     % A, B: rank must smaller or equal to 4, and the value is either 0 or 1
     % X: rank is at most min{rank(A), rank(B)}
     A=randi([0,1], 10, rk);
-    B=randi([0,1], rk, 5); 
+    B=randi([0,1], rk, 100); 
     X=A*B;
     rank_X=rank(X);
 end
@@ -25,4 +25,34 @@ d=mean(X, 2); % d means the column mean value of X
 X_centered=X-d; % X_centered means remove extra offset d from X
 
 % ---- Step 3: apply svd function to find pca(X) ----
+% the 'U' is orthogonal basis of original matrix X,
+% which we can use to build our 'C' in PCA
+[U, S, V]=svd(X_centered);
+% Select the first 4 columns of U to form the basis
+% U matrix is already sorted by default
+C=U(:, 1:4);
+% Compress X_centered to 4*100 Y
+Y=transpose(C)*X_centered;
 
+% ---- Step 4: reconstruct data to X_reconstruct via C, Y and d
+% Given equation: X_reconstruct=C*Y+d*ones(100)
+X_reconstruct=(C*Y)+(d*ones([1, 100]));
+
+% ---- Step 5: Plot C as curves
+% time vector with 10 sampling points
+t=linspace(1, 10, 10);
+% plot each column as one curve with 10 sampling points
+plot(t, C(:,1), t, C(:,2), t, C(:,3), t, C(:,4), 'LineWidth', 2)
+legend('Column1', 'Column2', 'Column3', 'Column4')
+xlabel('Sampling points')
+ylabel('Amplitude')
+title('Four curves of C matrix')
+
+% ---- Step 6: Plot X_reconstruct-X as 2D map
+X_error=X_reconstruct-X;
+figure;
+imagesc(X_error);
+colorbar;
+title('2D map of X-error')
+xlabel('X-axis')
+ylabel('Y-axis')
