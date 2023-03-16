bc5ba5e9c46aad3ca88bfc7042b48e69893a9058
urbao <E24094198@gs.ncku.edu.tw>
2023-03-16 10:19:00 +0800

    Finish compress img matrix into 2*L Y

--- PCA/part2/main2.m
+++ PCA/part2/main2.m
@@ -4,8 +4,14 @@ clc % clear the command window
 
 % ---- Step 1: read the image in ----
 img=imread('parrot.jpg'); % parrot: 1024*768=786432 pixels
-img_matrix=reshape(img, 3, []); % convert img to 3*L matrix
-[rows, cols]=size(img_matrix);
-fprintf("matrix size: ["+rows+", "+cols+"]\n");
+M=reshape(img, 3, []); % convert img to 3*L matrix
+M=double(matrix);
+[rows, cols]=size(M);
+fprintf("M size: ["+rows+", "+cols+"]\n");
 
-% ---- Step 2: apply PCA operation to the matrix ----
+% ---- Step 2: apply PCA operation to the M ----
+d=mean(M, 2);
+centered_M=matrix-d;
+[U, S, V]=svd(centered_M, 'econ');
+C=U(:, 1:2);
+Y=transpose(C)*centered_M;
