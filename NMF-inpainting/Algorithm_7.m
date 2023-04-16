ee1c2e1cbddf5a563769d675dc72bae284e06412
urbao <E24094198@gs.ncku.edu.tw>
2023-04-16 22:10:44 +0800

    commit NMF-inpainting for the first time - basic coding flow is written in comment

---
+++ NMF-inpainting/Algorithm_7.m
@@ -0,0 +1,13 @@
+% reset all windows and cmd window
+clc;
+close all;
+
+% generate data matrix Y_omega, and set missing entries to 0
+
+% apply NMF based inpainting technique to Y_omega data matrix
+
+% stored result data matrix to Data_7.mat
+
+% calculate the Frobenius norm of |Y-Y_hat| (check if inpainting is successful)
+
+% ??? maybe record some other useful info(ex. exec time; other photos)
